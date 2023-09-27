54adbbc3d438a9bc7941d56fd584aef9fb95cb67
Nizami Alijev <nizami.ee@mail.ru>
2023-09-27 19:06:07 +0300

    Initial commit

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
-
+mongoose.connect("mongodb+srv://admin-nizami:test123@cluster0.4yush.mongodb.net/blogDB", {useNewUrlParser: true});
 const postSchema = {
   title: String,
   content: String
